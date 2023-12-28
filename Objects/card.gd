d9819adab129e9eedd7691882cd593fe75d8d5a4
Tyler <tyler.shaffer24@gmail.com>
2023-12-28 00:07:38 -0800

    12/27 Added some basic card functionality

--- Objects/card.gd
+++ Objects/card.gd
@@ -10,7 +10,11 @@ const card_height = int(3.5 * 80)
 @onready var water_box = $CardBackground/VBoxContainer/HBoxContainer/VBoxContainer/WaterRequirement
 @onready var fert_box = $CardBackground/VBoxContainer/HBoxContainer/VBoxContainer/FertRequirement
 @onready var value_box = $CardBackground/VBoxContainer/HBoxContainer2/Value
+@onready var hover_shader = $HoverShader
+var selected_test = false
 
+signal card_selected
+signal card_hovered
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,8 +24,24 @@ func _ready():
 	water_box.text = str(card_stats.water_need)
 	fert_box.text = str(card_stats.fert_need)
 	value_box.text = str(card_stats.value)
-	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# FOR TESTING ONLY
+# Movement will be handled by Board
 func _process(delta):
-	pass
+	if selected_test:
+		global_position = lerp(global_position, get_global_mouse_position(), 25 * delta)
+
+func _on_card_hitbox_input_event(viewportNode, event: InputEvent, shape_idx: int) -> void:
+	if Input.is_action_just_pressed("left_click"):
+		selected_test = true
+		#print('clicked')
+		emit_signal("card_selected", self)
+
+func _on_card_hitbox_mouse_entered():
+	#print('hovered')
+	hover_shader.visible = true
+	emit_signal("card_hovered")
+
+
+func _on_card_hitbox_mouse_exited():
+	hover_shader.visible = false
--- UI/board.gd
+++ UI/board.gd
@@ -4,6 +4,8 @@ extends Node
 @onready var fert_counter = $FertilizerCounter
 @onready var gold_counter = $GoldCounter
 
+var selected_card = null
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	water_counter.update_title("Water")
@@ -12,23 +14,4 @@ func _ready():
 
 
 func _input(event):
-	if event is InputEventMouseButton:
-		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-			# Check if the mouse click is within the object's area
-			if get_child(0).rect_position.x <= event.position.x < get_child(0).rect_end.x and get_child(0).rect_position.y <= event.position.y < get_child(0).rect_end.y:
-				# Calculate the offset of the mouse click relative to the object's position
-				drag_offset = event.position - get_child(0).rect_position
-				is_dragging = true
-
-		elif event.released and event.button_index == BUTTON_LEFT:
-			is_dragging = false
-
-	elif event is InputEventMouseMotion and is_dragging:
-		# Update the object's position based on the mouse movement
-		get_child(0).rect_position = event.position - drag_offset
-
-	# Check if the object is dropped inside the container
-	if is_dragging and event is InputEventMouseButton and event.released and event.button_index == BUTTON_LEFT:
-		var container_rect = get_child(1).rect_global_position
-		if container_rect.x <= event.position.x < container_rect.x + get_child(1).rect_size.x and container_rect.y <= event.position.y < container_rect.y + get_child(1).rect_size.y:
-			print("Object dropped in the container!")
+	pass
